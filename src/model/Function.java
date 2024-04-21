e4046785e43c7e45231b47116ee13f182b9e50a8
alyssonlemes <alyssondiamantino@gmail.com>
2024-04-21 16:49:42 -0300

    Add function entity

---
+++ src/model/Function.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Function {
+    private int idFunction;
+    private String nameFunction;
+
+    public Function(int idFunction, String nameFunction) {
+        this.setIdFunction(idFunction);
+        this.setNameFunction(nameFunction);
+    }
+
+    public int getIdFunction() {
+        return idFunction;
+    }
+
+    public void setIdFunction(int idFunction) {
+        if(idFunction < 0){
+            throw new RuntimeException("Function id less than zero");
+        }
+        this.idFunction = idFunction;
+    }
+
+    public String getNameFunction() {
+        return nameFunction;
+    }
+
+    public void setNameFunction(String nameFunction) {
+        if (nameFunction.length() > 60){
+            throw new RuntimeException("Function name bigger than 60 chars");
+        }
+        this.nameFunction = nameFunction;
+    }
+}
