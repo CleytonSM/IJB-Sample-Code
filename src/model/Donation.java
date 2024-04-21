d56e1c7d1859b66408ae68b77963a46e5ff0a6a8
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-21 16:05:39 -0300

    Add donation entity

--- src/model/Donation.java
+++ src/model/Donation.java
@@ -1,2 +1,60 @@
-package model;public class Donation {
+package model;
+
+public class Donation {
+
+    private int idDonation;
+    private Donor donor;
+    private String dsDonation;
+    private Long qtDonation;
+
+    public Donation(int idDonation, Donor donor, String dsDonation, Long qtDonation) {
+        this.idDonation = idDonation;
+        this.donor = donor;
+        this.dsDonation = dsDonation;
+        this.qtDonation = qtDonation;
+    }
+
+    public int getIdDonation() {
+        return idDonation;
+    }
+
+    public void setIdDonation(int idDonation) {
+        if (idDonation <= 0) {
+            throw new RuntimeException("Id can't be less than 1");
+        }
+        this.idDonation = idDonation;
+    }
+
+    public Donor getDonor() {
+        return donor;
+    }
+
+    public void setDonor(Donor donor) {
+        if (donor == null) {
+            throw new RuntimeException("Donor can't be null");
+        }
+        this.donor = donor;
+    }
+
+    public String getDsDonation() {
+        return dsDonation;
+    }
+
+    public void setDsDonation(String dsDonation) {
+        if (dsDonation.length() > 100) {
+            throw new RuntimeException("Description length can't be more than 100 chars");
+        }
+        this.dsDonation = dsDonation;
+    }
+
+    public Long getQtDonation() {
+        return qtDonation;
+    }
+
+    public void setQtDonation(Long qtDonation) {
+        if(qtDonation < 1) {
+            throw new RuntimeException("Quantity can't be less than 1");
+        }
+        this.qtDonation = qtDonation;
+    }
 }
