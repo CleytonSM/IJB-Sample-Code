479edd5984f4780442f05e0640d3b4f783945296
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-21 15:10:50 -0300

    Add model package

---
+++ src/model/OutletProduct.java
@@ -0,0 +1,26 @@
+package model;
+
+import crud.CRUDOperation;
+
+public class OutletProduct implements CRUDOperation<OutletProduct> {
+
+    @Override
+    public void create(OutletProduct outletProduct) {
+
+    }
+
+    @Override
+    public void read(OutletProduct outletProduct) {
+
+    }
+
+    @Override
+    public void update(OutletProduct outletProduct) {
+
+    }
+
+    @Override
+    public void delete(OutletProduct outletProduct) {
+
+    }
+}
